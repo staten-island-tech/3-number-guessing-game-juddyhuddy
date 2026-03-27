76189c82d882caeb2d791bdd9e014182d60bf5eb
juddyhuddy <justinl732@nycstudents.net>
2026-03-27 08:49:35 -0400

    Update app.pp.py

--- app.pp.py
+++ app.pp.py
@@ -1,1 +1,12 @@
 weight = [2,3,4,4,5,6,1,2,2,2,1,8,2]
+
+y=0
+list = []
+y=0
+for i in weight:
+    if i%2 == 0:
+        y+= i
+    if 1 %2 == 1:
+        list.append(y)
+        y=0
+    print((list))
